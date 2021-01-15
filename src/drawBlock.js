e00366fda016997666e3844aeac7eccd4b12a8fd
damianprog <damianpukszta@gmail.com>
2021-01-15 17:29:10 +0100

    Changed colors

--- src/drawBlock.js
+++ src/drawBlock.js
@@ -3,6 +3,6 @@ export function drawBlock(ctx, position) {
 
     ctx.fillStyle = "#000";
     ctx.fillRect(position.x, position.y, blockSize, blockSize);
-    ctx.fillStyle = "#0d47a1";
+    ctx.fillStyle = "#0000ff"; 
     ctx.fillRect(position.x + 0.5, position.y + 0.5, blockSize - 1, blockSize - 1);
 }
